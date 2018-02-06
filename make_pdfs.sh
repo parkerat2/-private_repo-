497d4fd655d30eb2c8188d7dc93973513a23901c
rmparry7 <rmparry7@yahoo.com>
2018-02-06 17:26:02 -0500

    make pdfs

---
+++ make_pdfs.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+CWD=`pwd`
+VISUALS_DIR="./visuals"
+for d in $VISUALS_DIR/* ; do
+  DATA_PATH=`cat "$d/data_path"`
+  OUTPUTS=`cat "$d/output_files"`
+  TEX_FILE=./latex/`basename $d`.tex
+  echo "TEX_FILE=$TEX_FILE"
+  echo "DATA_PATH=$DATA_PATH"
+  echo "OUTPUTS=$OUTPUTS"
+ 
+  echo "\\documentclass{article}" > "$TEX_FILE"
+  echo "\\usepackage{minted}" >> "$TEX_FILE"
+  echo "\\usepackage{graphicx}" >> "$TEX_FILE"
+  echo "\\usepackage{verbatim}" >> "$TEX_FILE"
+  echo "\\begin{document}" >> "$TEX_FILE"
+  echo "    \\title{${d//_/\\_}}" >> "$TEX_FILE"
+  echo "    \\maketitle" >> "$TEX_FILE"
+  echo "    \\section{Description}" >> "$TEX_FILE"
+  echo "    \\small" >> "$TEX_FILE"
+  echo "    \\verbatiminput{../$d/README.md}" >> "$TEX_FILE"
+
+  for script in `find "$d" -name "*.[Ss][Hh]"`; do
+    DIR_NAME=`dirname $script`
+    BASE_NAME=`basename $script`
+
+    echo "    \\pagebreak" >> "$TEX_FILE"
+    echo "    \\section{${DIR_NAME//_/\\_}}" >> "$TEX_FILE"
+
+    for PYTHON_FILE in `find "$DIR_NAME" -name "*.[Pp][Yy]"`; do
+      echo "    \\subsection{${PYTHON_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\inputminted{python}{../$PYTHON_FILE}" >> "$TEX_FILE"
+    done
+    for R_FILE in `find "$DIR_NAME" -name "*.[Rr]"`; do
+      echo "    \\subsection{${R_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\inputminted{r}{../$R_FILE}" >> "$TEX_FILE"
+    done
+    for JAVA_FILE in `find "$DIR_NAME" -name "*.[Jj][Aa][Vv][Aa]"`; do
+      echo "    \\subsection{${JAVA_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\inputminted{java}{../$JAVA_FILE}" >> "$TEX_FILE"
+    done
+    for C_FILE in `find "$DIR_NAME" -name "*.[CcHh]"`; do
+      echo "    \\subsection{${C_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\inputminted{c}{../$C_FILE}" >> "$TEX_FILE"
+    done
+    for CPP_FILE in `find "$DIR_NAME" -name "*.[CcHh][Pp][Pp]"`; do
+      echo "    \\subsection{${CPP_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\inputminted{cpp}{../$CPP_FILE}" >> "$TEX_FILE"
+    done
+
+    echo "$DIR_NAME"
+    SKIP=1
+    for OUTPUT in $OUTPUTS ; do
+      OUTPUT_FILE="$DIR_NAME/$OUTPUT"
+      echo "    \\subsection{${OUTPUT_FILE//_/\\_}}" >> "$TEX_FILE"
+      echo "    \\includegraphics[width=\\textwidth]{../$OUTPUT_FILE}" >> "$TEX_FILE"
+    done
+  done
+  echo "\\end{document}" >> "$TEX_FILE"
+done
--- visuals/fig_1_47/README.md
+++ visuals/fig_1_47/README.md
@@ -1,8 +1,12 @@
 # Cars scatter plot
 
-Figure 1.47 in on page 45 of "Interactive Data Visualization: Foundations, Techniques, and Applications, Second Edition" by M. Ward, G. Grinstein, and D. Kim.
+"Interactive Data Visualization: Foundations, Techniques, and Applications, Second Edition" 
+by M. Ward, G. Grinstein, and D. Kim.
+Figure 1.47 in on page 45
 
-Original data retrieved from [here](http://www.idvbook.com/teaching-aid/data-sets/2004-cars-and-trucks-data/)
+Original data retrieved here:
+[http://www.idvbook.com/teaching-aid/data-sets/2004-cars-and-trucks-data/]
 
-Data converted to CSV with column headers available in this repository: [here](../../data/cars04.csv).
+Data converted to CSV with column headers available in this repository:
+[../../data/cars04.csv]
 
